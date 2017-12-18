3f975374909bad56c53becd23800eaa6bbd722a5
Sumit <sumityadavcools@gmail.com>
2017-12-18 21:31:55 +0530

    Neurons Init

--- digit_recognition.py
+++ digit_recognition.py
@@ -1,1 +1,12 @@
 from PIL import Image
+
+def main():
+	print("#"*15)
+	print("Training Phase")
+	print("#"*15)
+
+	# init neurons as an empty list
+	neurons = []
+
+if __name__ == "__main__":
+	main()
